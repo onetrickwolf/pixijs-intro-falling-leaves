881a42909de71330be419301efc9b8bccd89dd40
Brent Conn <brent.conn@gmail.com>
2021-09-18 20:20:56 -0700

    adding basic pixi demo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-console.log("Hello World!");
+import * as PIXI from 'pixi.js';
+
+const bunnyImage = require('../assets/bunny.png');
+
+const app = new PIXI.Application({
+    width: 800, height: 600, backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1,
+});
+
+document.body.appendChild(app.view);
+
+const container = new PIXI.Container();
+
+app.stage.addChild(container);
+
+// Create a new texture
+const texture = PIXI.Texture.from(bunnyImage);
+
+PIXI.Texture.from(bunnyImage);
+
+// Create a 5x5 grid of bunnies
+for (let i = 0; i < 25; i += 1) {
+    const bunny = new PIXI.Sprite(texture);
+    bunny.anchor.set(0.5);
+    bunny.x = (i % 5) * 40;
+    bunny.y = Math.floor(i / 5) * 40;
+    container.addChild(bunny);
+}
+
+// Move container to the center
+container.x = app.screen.width / 2;
+container.y = app.screen.height / 2;
+
+// Center bunny sprite in local container coordinates
+container.pivot.x = container.width / 2;
+container.pivot.y = container.height / 2;
+
+// Listen for animate update
+app.ticker.add((delta: number) => {
+    // rotate the container!
+    // use delta to create frame-independent transform
+    container.rotation -= 0.01 * delta;
+});
